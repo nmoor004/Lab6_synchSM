0d26b7f9706af2b65ef698247785471182c9bf39
Nathaniel Moore <nmoor004@wch136-05.cs.ucr.edu>
2019-10-18 16:59:08 -0700

    Initializing

--- source/main.c
+++ source/main.c
@@ -8,9 +8,42 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C Programmer should clear to 0.
+
+unsigned long _avr_timer_M = 1;	       // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;     // bit3 = 0: CTC mode (clear timer on compare)
+		          // bit2bit1bit0 = 011: pre-scaler /64
+			 // 00001011: 0x0B
+			// SO, 8 MHz clock or 8,000,000 / 64 = 125,000 ticks/s
+		       // Thus, TCNT1 register will count at 125,000 tick/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;    // Timer interrupt will be generated when TCNT1==OCR1A
+		       // We want a 1 ms tick. 0.001s * 125,000 ticks/s = 125
+		      // So when TCNT1 register equals 125,
+		     // 1 ms has passed. Thus, we compare to 125.
+
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x01; // bit1: OCIE1A -- enables compare match interrupt
+
+	// Initialize avr counter
+	TCNT1 = 0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	// Enable globla interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
