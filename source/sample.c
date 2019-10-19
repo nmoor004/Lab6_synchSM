0ae73fd857aa6997377d65909b4892ab3ae62283
nmoor004 <38025224+nmoor004@users.noreply.github.com>
2019-10-18 20:52:08 -0700

    Add files via upload

--- source/sample.c
+++ source/sample.c
@@ -12,11 +12,9 @@
 
 
 volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C Programmer should clear to 0.
-unsigned char global_PINA;
+
 unsigned long _avr_timer_M = 1;	       // Start count from here, down to 0. Default 1 ms.
 unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
-
-enum LED_States {LED_Init, Play, Youre_WINRAR} LED_State;
 
 void TimerOn() {
 	// AVR timer/counter controller register TCCR1
@@ -72,73 +70,18 @@ void TimerSet(unsigned long M) {
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-void Tick_LED() {
-	
-	switch(LED_State) {
-		case LED_Init:
-			PORTB = 0x01;
-			LED_State = Play;
-			break;
-
-		case Play:
-
-
-			if ((PORTB == 0x02) && (global_PINA == 0x00)) {
-				LED_State = Youre_WINRAR;
-				break;
-			} 
-
-			if (PORTB == 0x02 || PORTB == 0x01) {
-				PORTB = PORTB << 1;
-			}
-			else if (PORTB == 0x04) {
-				PORTB = 0x01;
-			}
-			break;
-		case Youre_WINRAR:
-			if (PINA == 0x00) {
-				LED_State = LED_Init;
-			}
-			break;
-	}
-
-	switch(LED_State) {
-
-		case LED_Init:
-			break;
-		case Play:
-			break;
-		case Youre_WINRAR:
-			break;
-	}
-
-
-
-}
-
 int main(void) {    /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0x01;  // input
-
-	DDRB = 0xFF; PORTB = 0x00; // output
-	DDRC = 0xFF; PORTC = 0x00;
-	TimerSet(300); // Timer Set to: 300 ms
+	DDRC = 0xFF;
+	PORTC = 0x00;
+	TimerSet(1000);
 	TimerOn();
-	LED_State = LED_Init;
-	
-	unsigned char toggle = 0;
+	unsigned char tmpB = 0x00;
     /* Insert your solution below */
     while (1) {
-	Tick_LED();
-	global_PINA = PINA;
-	while (!TimerFlag) {
-		if (toggle == 0) {
-			if (PINA == 0x00) {
-				global_PINA = PINA;
-				toggle = 1;
-			}
-		}
-	}; // Wait 1 sec
-	toggle = 0;
+	// User code (i.e. synchSM calls)
+	tmpB = ~tmpB; // Toggle PORTB; Temporary and bad programming style
+	PORTC = tmpB;
+	while (!TimerFlag); // Wait 1 sec
 	TimerFlag = 0;
  	 // Note: For the above a better style would use a synchSM with TickSM()
 	// This example just illusrates the use of ISR and flag.
